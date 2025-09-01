c2bae9be09723193af75c7ae6bdaac30222b09aa
Godstime Pious <godstimepious33@gmail.com>
2025-09-01 02:25:48 +0100

    Shopping Cart with Session Update

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-// 1. Simple Login & Profile
+// 2. Shopping Cart with Session
 
 import express from "express";
 import session from "express-session";
+import { nanoid } from "nanoid";
+
+import { checkAuth } from "./middlewares/session-auth.js";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -27,6 +30,7 @@ app.post("/login", (req, res) => {
   // check credentials
   if (findUser.password === password) {
     req.session.user = findUser;
+    req.session.user.cartItems = [];
     res.status(200).json({ message: `Login successful` });
     console.log(req.session);
   } else {
@@ -34,12 +38,36 @@ app.post("/login", (req, res) => {
   }
 });
 
-app.get("/profile", (req, res) => {
-  if (!req.session.user)
-    return res.status(401).json({ message: `Unauthorized` });
+app.get("/profile", checkAuth, (req, res) => {
   return res
     .status(200)
     .json({ message: `Username: ${req.session.user.username}` });
+});
+
+app.post("/api/product", checkAuth, (req, res) => {
+  const cartItem = { id: nanoid(), ...req.body };
+  req.session.user.cartItems.push(cartItem);
+  res.status(201).json({ message: `Item add to cart` });
+  console.log(req.session);
+});
+
+app.get("/api/cart", checkAuth, (req, res) => {
+  const session = req.session.user;
+  const sessionCart = session.cartItems;
+
+  if (sessionCart.length === 0)
+    return res.status(200).json({ message: "Your cart is empty" });
+
+  return res.status(200).json({ cart: sessionCart });
+});
+
+app.delete("/api/cart/:id", checkAuth, (req, res) => {
+  const { id } = req.params;
+  const session = req.session.user;
+  let sessionCart = session.cartItems;
+  const cartToDelete = sessionCart.filter((cart) => cart.id !== id);
+  session.cartItems = cartToDelete;
+  res.status(200).json({ message: `Cart ${id} deleted` });
 });
 
 app.get("/logout", (req, res) => {
